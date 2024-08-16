4198b92210f623a144f5a22d7ffcf97f3ff1c6a2
seokimun <gimun08@naver.com>
2024-08-17 00:08:35 +0900

    환경변수 설정

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const env = app.get(ConfigService);
+  const port = env.get('APP_PORT');
+
+  await app.listen(port);
+  console.log(`${port}번 실행중...`);
+}
+bootstrap();
