65f614fc128b94927f0834c48c4af9af8e028ebd
seokimun <gimun08@naver.com>
2024-08-20 16:28:27 +0900

    feat: ✨ 게시글 업데이트 기능추가

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post, Put } from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/CreatePost.dto';
 import { PostEntity } from '../entities/post.entity';
+import { UpdatePostDto } from './dto/UpdatePost.dto';
 
 @Controller('post')
 export class PostController {
@@ -10,6 +11,15 @@ export class PostController {
   @Post()
   async createPost(@Body() createPostDto: CreatePostDto): Promise<PostEntity> {
     const post = await this.postService.createPost(createPostDto);
+    return post;
+  }
+
+  @Put(':id')
+  async updatePost(
+    @Param('id') id: number,
+    @Body() updatePost: UpdatePostDto,
+  ): Promise<PostEntity> {
+    const post = await this.postService.updatePost(id, updatePost);
     return post;
   }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { PostEntity } from '../entities/post.entity';
 import { Repository } from 'typeorm';
 import { CreatePostDto } from './dto/CreatePost.dto';
+import { UpdatePostDto } from './dto/UpdatePost.dto';
 
 @Injectable()
 export class PostService {
@@ -14,5 +15,13 @@ export class PostService {
   async createPost(createPostDto: CreatePostDto): Promise<PostEntity> {
     const post = await this.postRepository.create(createPostDto);
     return this.postRepository.save(post);
+  }
+
+  async updatePost(
+    id: number,
+    updatePostDto: UpdatePostDto,
+  ): Promise<PostEntity> {
+    await this.postRepository.update(id, updatePostDto);
+    return this.postRepository.findOneBy({ id });
   }
 }
