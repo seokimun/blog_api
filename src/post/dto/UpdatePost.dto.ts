8d09864b8dcf0a93f7fa458e097dad99ce233290
seokimun <gimun08@naver.com>
2024-08-21 18:00:53 +0900

    refactor: ♻️ 업데이트 DTO파일 에러메세지 출력

--- src/post/dto/UpdatePost.dto.ts
+++ src/post/dto/UpdatePost.dto.ts
@@ -1,9 +1,13 @@
 import { IsString } from 'class-validator';
 
 export class UpdatePostDto {
-  @IsString()
+  @IsString({
+    message: 'title을 string 타입으로 입력하시오',
+  })
   title: string;
 
-  @IsString()
+  @IsString({
+    message: 'title을 string 타입으로 입력하시오',
+  })
   description: string;
 }
