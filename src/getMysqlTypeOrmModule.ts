1404ece11395a6aa99e2f122c81c7ddfde0dcbd4
seokimun <gimun08@naver.com>
2024-08-22 15:43:42 +0900

    refactor: ♻️ 터미널 로그기록 추가

--- src/getMysqlTypeOrmModule.ts
+++ src/getMysqlTypeOrmModule.ts
@@ -14,5 +14,6 @@ export const getMysqlTypeOrmModule = TypeOrmModule.forRootAsync({
     database: env.get<string>('DB_DATABASE'),
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
     synchronize: true,
+    logging: true,
   }),
 });
