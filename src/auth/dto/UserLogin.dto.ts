cfa10c39f94841cc3f038a1ddb66cb123df3cbbf
seokimun <gimun08@naver.com>
2024-09-22 02:41:11 +0900

    refactor: ♻️ dto코드 리팩토링

--- src/auth/dto/UserLogin.dto.ts
+++ src/auth/dto/UserLogin.dto.ts
@@ -1,11 +1,13 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class UserLoginDto {
-    @IsEmail()
+    @IsEmail({}, { message: 'email형식으로 입력하시오' })
     @IsNotEmpty()
     email: string;
 
-    @IsString()
+    @IsString({
+        message: 'password를 string형식으로 입력하시오',
+    })
     @IsNotEmpty()
     password: string;
 }
